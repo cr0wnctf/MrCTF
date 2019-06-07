7eb71104adefe9f50c5b17f94e9c25ef9b820573
Joshua <joshuamilkgreen@gmail.com>
2019-06-07 22:15:24 +0100

    Adds a run_errbot script

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 import logging
+import os
 
 # This is a minimal configuration to get you started with the Text mode.
 # If you want to connect Errbot to chat services, checkout
 # the options in the more complete config-template.py from here:
 # https://raw.githubusercontent.com/errbotio/errbot/master/errbot/config-template.py
 
-BACKEND = 'Text'  # Errbot will start in text mode (console only mode) and will answer commands from there.
+BACKEND = 'Discord'  # Errbot will start in text mode (console only mode) and will answer commands from there.
 
 BOT_DATA_DIR = r'./data'
 BOT_EXTRA_PLUGIN_DIR = r'./plugins'
@@ -16,3 +17,12 @@ BOT_LOG_LEVEL = logging.DEBUG
 AUTOINSTALL_DEPS = False
 
 BOT_ADMINS = ('@CHANGE_ME', )  # !! Don't leave that to "@CHANGE_ME" if you connect your errbot to a chat system !!
+
+try:
+    TOKEN = os.environ["DISCORD_TOKEN"]
+except KeyError as e:
+    raise ValueError("We need a Discord Token to connect!")
+
+BOT_IDENTITY = {
+    'token': TOKEN  # Discord Application Token
+}
