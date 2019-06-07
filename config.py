a128c62b11a1abcbddb5d9e4b90e79f8b2193aba
Joshua <joshuamilkgreen@gmail.com>
2019-06-07 17:45:06 +0100

    config updates

--- config.py
+++ config.py
@@ -7,10 +7,10 @@ import logging
 
 BACKEND = 'Text'  # Errbot will start in text mode (console only mode) and will answer commands from there.
 
-BOT_DATA_DIR = r'/home/joshua/projects/chatbot2/data'
-BOT_EXTRA_PLUGIN_DIR = r'/home/joshua/projects/chatbot2/plugins'
+BOT_DATA_DIR = r'./data'
+BOT_EXTRA_PLUGIN_DIR = r'./plugins'
 
-BOT_LOG_FILE = r'/home/joshua/projects/chatbot2/errbot.log'
+BOT_LOG_FILE = r'./errbot.log'
 BOT_LOG_LEVEL = logging.DEBUG
 
 BOT_ADMINS = ('@CHANGE_ME', )  # !! Don't leave that to "@CHANGE_ME" if you connect your errbot to a chat system !!
